533abbeea7f01d59ab9038014709390d66da5dab
Deirdre Saoirse Moen <deirdre@deirdre.net>
2020-12-26 11:34:37 -0800

    Issues to work on:
    1. Vertical padding isn't.
    2. Result field is centered (why?)
    3. Bottom row with 2x button is annoyingly off by one pixel.

---
+++ Calculator/Color+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Deirdre Saoirse Moen on 12/26/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let numberButton = Color("NumberButton")
+    static let operatorButton = Color("OperatorButton")
+    static let utilityButton = Color("UtilityButton")
+    static let resultView = Color("ResultView")
+    
+    static let foregroundColor = Color("ForegroundColor")
+    static let backgroundColor = Color("BackgroundColor")
+    static let textColor = Color("TextColor")
+}
--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,10 +7,81 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ButtonCell: View {
+    let color: Color
+    let text: String
+    let width: Int
+    let geometry: GeometryProxy
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(text)
+            .frame(width: geometry.size.width / 4.0 * CGFloat(width), height: geometry.size.height)
+            .background(color)
+            .foregroundColor(Color.textColor)
+            .font(.title)
+            .padding(1)
+     }
+}
+
+struct ContentView: View {
+    
+    let blue = Color(red: 27, green: 173, blue: 248)
+    let orange = Color.orange
+    let grey = Color.gray
+
+    var body: some View {
+        ZStack {
+            Color.backgroundColor
+
+        VStack(spacing: 0) {
+            HStack {
+                Spacer()
+                GeometryReader { geometry in
+                    ButtonCell(color: Color.resultView, text: "0", width: 4, geometry: geometry)
+             }
+            }
+            GeometryReader { geometry in
+                HStack(spacing: 0) {
+                    ButtonCell(color: Color.utilityButton, text: "%", width: 1, geometry: geometry)
+                ButtonCell(color: Color.utilityButton, text: "+/-", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.utilityButton, text: "AC", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.operatorButton, text: "÷", width: 1, geometry: geometry)
+                }
+            }
+            GeometryReader { geometry in
+                HStack(spacing: 0) {
+                ButtonCell(color: Color.numberButton, text: "7", width: 1, geometry: geometry)
+                ButtonCell(color: Color.numberButton, text: "8", width: 1, geometry: geometry)
+                ButtonCell(color: Color.numberButton, text: "9", width: 1, geometry: geometry)
+                ButtonCell(color: Color.operatorButton, text: "×", width: 1, geometry: geometry)
+                }
+            }
+            GeometryReader { geometry in
+                HStack(spacing: 0) {
+                ButtonCell(color: Color.numberButton, text: "4", width: 1, geometry: geometry)
+                ButtonCell(color: Color.numberButton, text: "5", width: 1, geometry: geometry)
+                ButtonCell(color: Color.numberButton, text: "6", width: 1, geometry: geometry)
+                ButtonCell(color: Color.operatorButton, text: "-", width: 1, geometry: geometry)
+                }
+            }
+            GeometryReader { geometry in
+                HStack(spacing: 0) {
+                ButtonCell(color: Color.numberButton, text: "1", width: 1, geometry: geometry)
+                ButtonCell(color: Color.numberButton, text: "2", width: 1, geometry: geometry)
+                ButtonCell(color: Color.numberButton, text: "3", width: 1, geometry: geometry)
+                ButtonCell(color: orange, text: "+", width: 1, geometry: geometry)
+                }
+            }
+            GeometryReader { geometry in
+                HStack(spacing: 0) {
+                ButtonCell(color: Color.numberButton, text: "0", width: 2, geometry: geometry)
+                ButtonCell(color: Color.numberButton, text: ".", width: 1, geometry: geometry)
+                ButtonCell(color: Color.operatorButton, text: "=", width: 1, geometry: geometry)
+//                    .frame(width: geometry.size.width / 4, height: geometry.size.height / 6)
+                }
+            }
+        }
+        }
     }
 }
 
