77ebeee199c22c0b0eb21159239b061f15740e3e
Deirdre Saoirse Moen <deirdre@deirdre.net>
2020-12-26 14:15:56 -0800

    1. Added landscape mode to 2nd preview. 2. Removed old code that was commented out. 3. Fixed indenting.

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+// credit for landscape to: https://medium.com/@prafullkumar77/swiftui-managing-previews-8b0fb3b59b85
+
+struct Landscape<Content>: View where Content: View {
+    let content: () -> Content
+    let height = UIScreen.main.bounds.width //toggle width height
+    let width = UIScreen.main.bounds.height
+
+    var body: some View {
+     content().previewLayout(PreviewLayout.fixed(width: width, height: height))
+    }
+}
+
 struct ResultCell: View {
     let color: Color
     let text: String
@@ -54,57 +66,62 @@ struct ContentView: View {
         ZStack {
             Color.backgroundColor
 
-        VStack(spacing: 0) {
+            VStack(spacing: 0) {
+                    GeometryReader { geometry in
+                        ResultCell(color: Color.resultView, text: "0", geometry: geometry)
+                }
                 GeometryReader { geometry in
-                    ResultCell(color: Color.resultView, text: "0", geometry: geometry)
-            }
-            GeometryReader { geometry in
-                HStack(spacing: 0) {
-                    ButtonCell(color: Color.utilityButton, text: "%", accessibleLabel: "percent", width: 1, geometry: geometry)
-                ButtonCell(color: Color.utilityButton, text: "+/-", accessibleLabel: "plus/minus", width: 1, geometry: geometry)
-                    ButtonCell(color: Color.utilityButton, text: "AC", accessibleLabel: "clear", width: 1, geometry: geometry)
-                    ButtonCell(color: Color.operatorButton, text: "÷", accessibleLabel: "divide", width: 1, geometry: geometry)
+                    HStack(spacing: 0) {
+                        ButtonCell(color: Color.utilityButton, text: "%", accessibleLabel: "percent", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.utilityButton, text: "+/-", accessibleLabel: "plus/minus", width: 1, geometry: geometry)
+                        ButtonCell(color: Color.utilityButton, text: "AC", accessibleLabel: "clear", width: 1, geometry: geometry)
+                        ButtonCell(color: Color.operatorButton, text: "÷", accessibleLabel: "divide", width: 1, geometry: geometry)
+                    }
+                }
+                GeometryReader { geometry in
+                    HStack(spacing: 0) {
+                    ButtonCell(color: Color.numberButton, text: "7", accessibleLabel: "7", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.numberButton, text: "8", accessibleLabel: "8", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.numberButton, text: "9", accessibleLabel: "9", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.operatorButton, text: "×", accessibleLabel: "multiply", width: 1, geometry: geometry)
+                    }
+                }
+                GeometryReader { geometry in
+                    HStack(spacing: 0) {
+                    ButtonCell(color: Color.numberButton, text: "4", accessibleLabel: "4", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.numberButton, text: "5", accessibleLabel: "5", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.numberButton, text: "6", accessibleLabel: "6", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.operatorButton, text: "-", accessibleLabel: "subtract", width: 1, geometry: geometry)
+                    }
+                }
+                GeometryReader { geometry in
+                    HStack(spacing: 0) {
+                    ButtonCell(color: Color.numberButton, text: "1", accessibleLabel: "1", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.numberButton, text: "2", accessibleLabel: "2", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.numberButton, text: "3", accessibleLabel: "3", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.operatorButton, text: "+", accessibleLabel: "plus", width: 1, geometry: geometry)
+                    }
+                }
+                GeometryReader { geometry in
+                    HStack(spacing: 0) {
+                    ButtonCell(color: Color.numberButton, text: "0", accessibleLabel: "0", width: 2, geometry: geometry)
+                    ButtonCell(color: Color.numberButton, text: ".", accessibleLabel: "decimal point", width: 1, geometry: geometry)
+                    ButtonCell(color: Color.operatorButton, text: "=", accessibleLabel: "=", width: 1, geometry: geometry)
+                    }
                 }
             }
-            GeometryReader { geometry in
-                HStack(spacing: 0) {
-                ButtonCell(color: Color.numberButton, text: "7", accessibleLabel: "7", width: 1, geometry: geometry)
-                ButtonCell(color: Color.numberButton, text: "8", accessibleLabel: "8", width: 1, geometry: geometry)
-                ButtonCell(color: Color.numberButton, text: "9", accessibleLabel: "9", width: 1, geometry: geometry)
-                ButtonCell(color: Color.operatorButton, text: "×", accessibleLabel: "multiply", width: 1, geometry: geometry)
-                }
-            }
-            GeometryReader { geometry in
-                HStack(spacing: 0) {
-                ButtonCell(color: Color.numberButton, text: "4", accessibleLabel: "4", width: 1, geometry: geometry)
-                ButtonCell(color: Color.numberButton, text: "5", accessibleLabel: "5", width: 1, geometry: geometry)
-                ButtonCell(color: Color.numberButton, text: "6", accessibleLabel: "6", width: 1, geometry: geometry)
-                ButtonCell(color: Color.operatorButton, text: "-", accessibleLabel: "subtract", width: 1, geometry: geometry)
-                }
-            }
-            GeometryReader { geometry in
-                HStack(spacing: 0) {
-                ButtonCell(color: Color.numberButton, text: "1", accessibleLabel: "1", width: 1, geometry: geometry)
-                ButtonCell(color: Color.numberButton, text: "2", accessibleLabel: "2", width: 1, geometry: geometry)
-                ButtonCell(color: Color.numberButton, text: "3", accessibleLabel: "3", width: 1, geometry: geometry)
-                ButtonCell(color: Color.operatorButton, text: "+", accessibleLabel: "plus", width: 1, geometry: geometry)
-                }
-            }
-            GeometryReader { geometry in
-                HStack(spacing: 0) {
-                ButtonCell(color: Color.numberButton, text: "0", accessibleLabel: "0", width: 2, geometry: geometry)
-                ButtonCell(color: Color.numberButton, text: ".", accessibleLabel: "decimal point", width: 1, geometry: geometry)
-                ButtonCell(color: Color.operatorButton, text: "=", accessibleLabel: "=", width: 1, geometry: geometry)
-//                    .frame(width: geometry.size.width / 4, height: geometry.size.height / 6)
-                }
-            }
-        }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            Landscape {
+                ContentView()
+                    .preferredColorScheme(.dark)
+            }
+        }
     }
 }
